1c446a0abc3c4fcea44b9b04ab2166f050df5e05
alamgir8 <ahossain38900@gmail.com>
2023-06-06 19:16:52 +0600

    feat(v-1): added radio button

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -4,6 +4,8 @@ import React from "react";
 import Button from "../components/Button";
 import Input from "../components/Input";
 
+const genderOptions = ["Male", "Female"];
+
 const SignUp = ({ navigation }) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -12,6 +14,16 @@ const SignUp = ({ navigation }) => {
         <Input placeholder="Email Address" />
         <Input placeholder="Age" />
         <Input placeholder="Password" secureTextEntry />
+        {genderOptions.map((option) => (
+          <Pressable key={option} style={styles.radioContainer}>
+            <View style={[styles.outerCircle, styles.selectedOuterCircle]}>
+              <View
+                style={[styles.innerCircle, styles.selectedInnerCircle]}
+              ></View>
+            </View>
+            <Text style={styles.radioText}>{option}</Text>
+          </Pressable>
+        ))}
       </View>
       <View
         style={{
@@ -39,4 +51,34 @@ const SignUp = ({ navigation }) => {
 
 export default SignUp;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  radioContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  outerCircle: {
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "#cfcfcf",
+    marginRight: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  innerCircle: {
+    height: 15,
+    width: 15,
+    borderRadius: 7.5,
+    borderWidth: 1,
+    borderColor: "#cfcfcf",
+  },
+  selectedOuterCircle: {
+    borderColor: "orange",
+  },
+  selectedInnerCircle: {
+    borderColor: "orange",
+    backgroundColor: "orange",
+  },
+});
